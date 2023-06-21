7a0eab0fa9e30da57a5c956fab566d8e1ad428da
JstVex <heinhtetlulu14@gmail.com>
2023-06-21 23:08:42 +0630

    Remove ownerId as owner points to User

--- controllers/groupController.js
+++ controllers/groupController.js
@@ -8,10 +8,10 @@ const getGroups = async (req, res) => {
 
 // create a new group 
 const createGroup = async (req, res) => {
-    const { title, description, owner, ownerId, time, date, place, groupLink, members, events } = req.body;
+    const { title, description, owner, time, date, place, groupLink, members, events } = req.body;
 
     try {
-        const group = await Group.create({ title, description, owner, ownerId, time, date, place, groupLink, members, events })
+        const group = await Group.create({ title, description, owner, time, date, place, groupLink, members, events })
         res.status(200).json(group)
     } catch (error) {
         res.status(400).json({ error: error.message })
