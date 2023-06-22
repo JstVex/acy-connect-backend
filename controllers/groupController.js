7af2b021c6dfa090b4f2a0cb75b7f552b1fd3f99
JstVex <heinhtetlulu14@gmail.com>
2023-06-22 23:26:59 +0630

    Implement 'user joining a new group' function and update both group and user
    Create a new controller that update the group's members array when a new user is joined and also update that user's groups array

--- controllers/groupController.js
+++ controllers/groupController.js
@@ -1,4 +1,5 @@
 const Group = require("../models/Groups");
+const User = require("../models/Users");
 
 // get all groups
 const getGroups = async (req, res) => {
@@ -12,13 +13,50 @@ const createGroup = async (req, res) => {
 
     try {
         const group = await Group.create({ title, description, owner, time, date, place, groupLink, members, events })
-        res.status(200).json(group)
+
+        const updatedUser = await User.findOneAndUpdate({
+            _id: owner
+        }, {
+            $addToSet: { groups: group._id }
+        },
+            { new: true }
+        )
+        res.status(200).json({ group, updatedUser });
     } catch (error) {
         res.status(400).json({ error: error.message })
     }
 }
 
+// update when a new user join
+const userJoinGroup = async (req, res) => {
+    const { user, id } = req.body;
+
+    try {
+        const updatedGroup = await Group.findOneAndUpdate({
+            _id: id
+        }, {
+            $addToSet: { members: user }
+        },
+            { new: true }
+        )
+
+        const updatedUser = await User.findOneAndUpdate({
+            _id: user
+        }, {
+            $addToSet: { groups: id }
+        },
+            { new: true }
+        )
+
+        res.status(200).json({ updatedGroup, updatedUser });
+
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+}
+
 module.exports = {
     getGroups,
-    createGroup
+    createGroup,
+    userJoinGroup
 }
