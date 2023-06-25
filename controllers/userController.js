7f2c33b4321040300e3d8cb5a766b3bb26849495
JstVex <heinhtetlulu14@gmail.com>
2023-06-25 23:18:21 +0630

    Add two new functions
    Get users who aren't connected to the current user and get one user by id

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("../models/Users");
+const Connection = require("../models/Connections")
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const asyncHandler = require('express-async-handler');
@@ -21,6 +22,60 @@ const getUsersExceptCurrent = asyncHandler(async (req, res) => {
     }
 })
 
+// get users not connected to the current user
+const getUsersNotConnected = async (req, res) => {
+    const userId = req.query.userId;
+
+    try {
+        // Find the current user
+        const currentUser = await User.findById(userId);
+        console.log('currentuser is', currentUser)
+
+        // Get the IDs of the users the current user has already connected with
+        const connectedUserIds = currentUser.connections.map(connection => connection.toString());
+        console.log('connetedUserIds', connectedUserIds)
+
+        // Find all users who are not in the connectedUserIds array
+        const unconnectedUsers = await User.find({
+            _id: { $nin: [userId, ...connectedUserIds] }
+        });
+
+        console.log('unconnectedusers', unconnectedUsers)
+
+        res.status(200).json(unconnectedUsers);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+// const getUsersNotConnected = async (req, res) => {
+//     const userId = req.query.userId;
+
+//     try { 
+//         const user = await User.findById(userId).p
+//         console.log('user is', user)
+
+//         const connections = await Connection.findOne({
+//             user: userId
+//         }).populate('connections');
+//         console.log('connections', connections)
+
+//         // Get the IDs of the connected users
+//         // const connectedUserIds = user.connections.map((connection) => connection.users);
+//         const connectedUserIds = await connections.map((connection) => connection._id).populate('users');
+//         console.log('connected users are', connectedUserIds)
+
+//         // Find all users who are not connected to the current user
+//         const usersNotConnected = await User.find({
+//             _id: { $ne: userId },
+//             connections: { $nin: connectedUserIds },
+//         });
+
+//         res.status(200).json(usersNotConnected);
+//     } catch (error) {
+//         res.status(500).json({ error: error.message });
+//     }
+// }
+
 // get current user
 const getMe = asyncHandler(async (req, res) => {
     // const { _id, name, email, image, fblink, hobbies, activeDay, groups } = await User.findById(req.user.id);
@@ -30,6 +85,17 @@ const getMe = asyncHandler(async (req, res) => {
     //     name,
     //     email
     // });
+    res.status(200).json(user);
+})
+
+// get one user 
+const getUser = asyncHandler(async (req, res) => {
+    const { userId } = req.params;
+
+    const user = await User.findById({
+        _id: userId
+    }).populate('groups').populate('connections');
+
     res.status(200).json(user);
 })
 
@@ -122,7 +188,9 @@ const generateToken = (id) => {
 module.exports = {
     getUsers,
     getUsersExceptCurrent,
+    getUsersNotConnected,
     getMe,
+    getUser,
     registerUser,
     loginUser,
     updateUser
