7ddd31c29a0d0b179b93a9735d2a6e6fb7392b45
JstVex <heinhtetlulu14@gmail.com>
2023-06-28 02:14:40 +0630

    Created Event model

---
+++ models/Events.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema
+
+const eventSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    date: {
+        type: Date
+    },
+    time: {
+        type: String
+    },
+    group: {
+        type: Schema.Types.ObjectId,
+        ref: "Group"
+    },
+    participants: [{
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }],
+})
+
+module.exports = mongoose.model('Event', eventSchema);
