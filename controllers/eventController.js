08a63bed99ef3e14e8e5b5c60553b6593f534477
JstVex <heinhtetlulu14@gmail.com>
2023-07-02 22:05:21 +0630

    Create get, update and remove controllers

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,5 +1,6 @@
 const Event = require("../models/Events");
 const Group = require("../models/Groups");
+const User = require("../models/Users");
 
 // get all events
 const getEvents = async (req, res) => {
@@ -25,7 +26,6 @@ const getEventsForGroup = async (req, res) => {
 // create a new event 
 const createEvent = async (req, res) => {
     const { title, description, date, time, group } = req.body;
-    console.log('group id is', group)
 
     try {
         const event = await Event.create({ title, description, date, time, group });
@@ -44,8 +44,96 @@ const createEvent = async (req, res) => {
     }
 }
 
+// update participants when a new user participates
+const updateParticipants = async (req, res) => {
+    const { eventId } = req.params;
+    const { userId } = req.body;
+
+    try {
+        const updatedEvent = await Event.findOneAndUpdate({
+            _id: eventId
+        }, {
+            $addToSet: { participants: userId }
+        },
+            { new: true }
+        );
+
+        const updatedUser = await User.findOneAndUpdate({
+            _id: userId
+        }, {
+            $addToSet: { events: eventId }
+        }, {
+            new: true
+        })
+
+        res.status(200).json(updatedEvent)
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+}
+
+// remove user from participants when they quit an event
+const removeParticipant = async (req, res) => {
+    const { eventId } = req.params;
+    const { userId } = req.body;
+
+    try {
+        const updatedEvent = await Event.findOneAndUpdate({
+            _id: eventId
+        }, {
+            $pull: { participants: userId }
+        },
+            { new: true }
+        );
+
+        const updatedUser = await User.findOneAndUpdate({
+            _id: userId
+        }, {
+            $pull: { events: eventId }
+        },
+            { new: true }
+        );
+
+        res.status(200).json(updatedEvent);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+// get events for all the groups user has joined
+const getEventsForAllGroups = async (req, res) => {
+    const { userId } = req.params;
+
+    try {
+        // Retrieve the user document with populated groups and events field
+        const user = await User.findById(userId).populate('groups').populate('events');
+
+        // Get an array of group IDs from the user's groups field
+        const groupIds = user.groups.map(group => group._id);
+
+        // Get an array of event IDs that the user has already participated in
+        const participatedEventIds = user.events.map(event => event._id);
+
+        // Find events where the group field matches any of the group IDs
+        const events = await Event.find(
+            {
+                _id: { $nin: participatedEventIds },
+                group: { $in: groupIds }
+            }
+        );
+
+        res.status(200).json(events);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+
 module.exports = {
     getEvents,
     getEventsForGroup,
-    createEvent
+    getEventsForAllGroups,
+    createEvent,
+    updateParticipants,
+    removeParticipant
 }
