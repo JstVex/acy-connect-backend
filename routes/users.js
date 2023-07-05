10bb577f0631c6921dd556d7b7ff2bea46fb43cf
JstVex <heinhtetlulu14@gmail.com>
2023-07-06 00:09:11 +0630

    Set routes for sending friend request and group invitation

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,8 @@ const {
     getMutualFriends,
     registerUser,
     loginUser,
+    sendConnectionRequest,
+    sendGroupInvitation,
     updateUser
 } = require("../controllers/userController");
 
@@ -25,12 +27,16 @@ router.get('/me', protect, getMe);
 
 router.get('/:userId', getUser);
 
-router.get('/mutualfriends/:userId/:groupId', getMutualFriends)
+router.get('/mutualfriends/:userId/:groupId', getMutualFriends);
 
 router.post('/', registerUser);
 
 router.post('/login', loginUser);
 
-router.patch('/me', updateUser)
+router.patch('/friendrequest', sendConnectionRequest);
+
+router.patch('/groupinvitation', sendGroupInvitation);
+
+router.patch('/me', updateUser);
 
 module.exports = router;
